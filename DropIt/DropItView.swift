59137c4fb3d0ed3bc0df31a51f23278495e30d83
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-07-13 14:40:04 -0700

    added random drops on tap working

---
+++ DropIt/DropItView.swift
@@ -0,0 +1,94 @@
+//
+//  DropItView.swift
+//  DropIt
+//
+//  Created by jeffrey dinh on 7/13/16.
+//  Copyright © 2016 jeffrey dinh. All rights reserved.
+//
+
+import UIKit
+
+class DropItView: UIView {
+    private let dropsPerRow = 10
+    
+    private var dropSize: CGSize {
+        let size = bounds.size.width / CGFloat(dropsPerRow)
+        return CGSize(width: size, height: size)
+    }
+    
+    func addDrop() {
+        var frame = CGRect(origin: CGPoint.zero, size: dropSize)
+        frame.origin.x = CGFloat.random(dropsPerRow) * dropSize.width
+        
+        let drop = UIView(frame: frame)
+        drop.backgroundColor = UIColor.random
+        
+        addSubview(drop)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+}
---
+++ DropIt/DropItViewController.swift
@@ -0,0 +1,73 @@
+//
+//  DropItViewController.swift
+//  DropIt
+//
+//  Created by jeffrey dinh on 7/13/16.
+//  Copyright © 2016 jeffrey dinh. All rights reserved.
+//
+
+import UIKit
+
+class DropItViewController: UIViewController {
+
+    @IBOutlet weak var gameView: DropItView! {
+        didSet {
+            gameView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(addDrop(_:))))
+        }
+    }
+    
+    func addDrop(recognizer: UITapGestureRecognizer) {
+        if recognizer.state == .Ended {
+            gameView.addDrop()
+        }
+    }
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
